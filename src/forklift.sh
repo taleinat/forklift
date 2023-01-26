0d4ca5c3a53fee43c16342d9e2bf64a7551d7a97
Tal Einat <tal@rhinohealth.com>
2023-01-26 09:03:23 +0200

    Add pid file and port file

--- src/forklift.sh
+++ src/forklift.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 set -eu -o pipefail
-hostname="$1"
-shift
-port="$1"
-shift
 
-exec 3<>"/dev/tcp/$hostname/$port"  # open tcp connection
+runtime_dir="${XDG_RUNTIME_DIR:-${TMPDIR:-/tmp}}"
+shopt -s nullglob
+service_runtime_dirs=("$runtime_dir/forklift-$USER"-??????)
+shopt -u nullglob
+echo "${service_runtime_dirs[@]}"
+if [[ ${#service_runtime_dirs[@]} -eq 0 ]]; then
+  echo "Service not running." >&2
+  exit 1
+elif [[ ${#service_runtime_dirs[@]} -gt 1 ]]; then
+  echo "Multiple service runtime dirs, argh!" >&2
+  exit 1
+fi
+service_runtime_dir="${service_runtime_dirs[0]}"
+
+IFS= read -r port <"$service_runtime_dir/black.port"
+exec 3<>"/dev/tcp/127.0.0.1/$port"  # open tcp connection
 echo "$@" >&3
 #exec 3<&0  # redirect stdin into tcp connection
 
--- src/forklift/__main__.py
+++ src/forklift/__main__.py
@@ -1,11 +1,17 @@
 import io
 import os
+import random
 import shlex
 import socket
+import string
 import sys
 import time
+from pathlib import Path
 from typing import cast, Any, BinaryIO, Union
 
+from .utils import pid_exists
+
+# TODO: Make this generic.
 from black import patched_main as main
 
 
@@ -40,6 +46,29 @@ class _SocketWriter(io.BufferedIOBase):
         return self._sock.fileno()
 
 
+def get_service_runtime_dir_path() -> Path:
+    runtime_dir_path = Path(
+        os.getenv("XDG_RUNTIME_DIR") or os.getenv("TMPDIR") or "/tmp"
+    )
+    service_runtime_dirs = list(
+        runtime_dir_path.glob(f"forklift-{os.getenv('USER')}-??????")
+    )
+    if service_runtime_dirs:
+        if len(service_runtime_dirs) > 1:
+            raise Exception("TODO")
+        service_runtime_dir = service_runtime_dirs[0]
+    else:
+        random_part = "".join(
+            [random.choice(string.ascii_uppercase) for _i in range(6)]
+        )
+        service_runtime_dir = (
+            runtime_dir_path / f"forklift-{os.getenv('USER')}-{random_part}"
+        )
+        service_runtime_dir.mkdir(exist_ok=False, mode=0o700)
+
+    return service_runtime_dir
+
+
 if __name__ == "__main__":
     # # Do the double-fork dance to daemonize.
     # print(f"{os.getpid()=}, {os.getpgid(0)=}, {os.getsid(0)=}")
@@ -60,19 +89,41 @@ if __name__ == "__main__":
     #
     # print(f"{os.getpid()=}, {os.getpgid(0)=}, {os.getsid(0)=}")
 
+    service_runtime_dir_path = get_service_runtime_dir_path()
+    pid_file_path = service_runtime_dir_path / "black.pid"
+    port_file_path = service_runtime_dir_path / "black.port"
+
+    # Set up pid file.
+    if pid_file_path.exists():
+        file_pid = int(pid_file_path.read_text())
+        if pid_exists(file_pid):
+            raise Exception("Forklift process already exists.")
+    pid = os.getpid()
+    pid_file_path.write_text(f"{pid}\n")
+
+    # Open socket.
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host, port = "127.0.0.1", 9998
-    sock.bind((host, port))
+    sock.bind(("127.0.0.1", 0))
+    host, port = sock.getsockname()
+    port_file_path.write_text(f"{port}\n")
+
+    # Listen for connections.
     try:
         sock.listen()
-        print(f"Listening on {host}:{port} ...")
+        print(f"Listening on {host}:{port} (pid={pid}) ...")
         while True:
             conn, address = sock.accept()
             print(f"Got connection from: {address}")
             if os.fork() == 0:
                 break
     except:
+        # Cleanup upon daemon shutdown.
         sock.close()
+        if pid_file_path.exists():
+            file_pid = int(pid_file_path.read_text())
+            if file_pid == pid:
+                pid_file_path.unlink(missing_ok=True)
+                port_file_path.unlink(missing_ok=True)
         raise
 
     # TODO: Need to do something like the double-fork dance to decouple from the parent process?
@@ -93,9 +144,12 @@ if __name__ == "__main__":
         print(f"child proc started", file=sys.__stdout__)
         start = time.monotonic()
         sys.argv[1:] = shlex.split(rfile.readline().strip().decode())
-        # TODO: Make this generic.
-        main()
-        print("child proc ended", file=sys.__stdout__)
+        try:
+            # TODO: Make this generic.
+            main()
+        finally:
+            print("child proc ended", file=sys.__stdout__)
+            print(f"Time: {time.monotonic() - start:.3f}", file=sys.__stdout__)
     else:
         try:
             _pid, wait_status = os.waitpid(pid, 0)
